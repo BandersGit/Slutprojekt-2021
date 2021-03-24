d2682c026517e8d34fbc5bbf8352e331f56643d5
BandersGit <andy.i.flaskan@gmail.com>
2021-03-24 11:05:54 +0100

    Created class files

--- Slutprojekt/Program.cs
+++ Slutprojekt/Program.cs
@@ -6,6 +6,7 @@ namespace Slutprojekt
     {
         static void Main(string[] args)
         {
+            //Clone academy, Base clones get trained to become different types (ARC, Jet, Commando, Heavy)
             Console.WriteLine("Hello World!");
         }
     }
